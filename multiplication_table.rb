96a4111d051628e2b01a2bdd0cf435c3d23520bb
Erik Isaksen <erik_isaksen@hotmail.com>
2012-02-06 11:29:36 -0500

    fixed hash logic for multi table

--- multiplication_table.rb
+++ multiplication_table.rb
@@ -3,28 +3,50 @@
 class MultiplicationTable
   
   def initialize (rows_and_cols, header, decorate)
+    base_scale(rows_and_cols)
+    @header = header.empty? ? "" : header   
+    @decorate = decorate.empty? ? false : true
+    make_h(rows_and_cols)
+  end
+
+  def base_scale(rows_and_cols)
+    unless Integer(rows_and_cols).is_a? Integer
+      puts "Program is exiting. Please enter a valid integer for the Scale"
+      exit
+    end
     @rows_and_cols = rows_and_cols
-    @header = header
-    @decorate = decorate || false
-  end
-  
-  def make_multi_tbl
-    @multiplication_table = Hash.new()
   end
 
-  def has_header?(hdr) 
-    hdr.present?
+  def make_h(max)
+    @multiplication_table = {}
+    add_header
+    make_h_rows
   end
 
-  def decorate
-
+  def make_h_rows
+    count = 0
+    while (count < Integer(@rows_and_cols))
+      count += 1    
+      @multiplication_table.merge!({"row#{count}" => []})
+      update_h(count)
+    end
+    puts "#{@multiplication_table}"
   end
-
+	
   def add_header
-
+    @multiplication_table.merge!({"header" => @header}) unless (@header == "")
   end
 
-  def print_table
+  def update_h(count)
+    count2 = 0
+    row_values = []
+    until (count2 == Integer(@rows_and_cols))
+      count2 += 1
+      @multiplication_table["row#{count}"] = row_values << (count*count2)
+    end
+  end
+
+  def h_to_tbl
 
   end
  
@@ -41,3 +63,4 @@ header = gets.chomp
 puts "is the table going to be decorated (true or false)"
 decorate = gets.chomp
 
+m_tbl = MultiplicationTable.new(rows_and_cols, header, decorate)
