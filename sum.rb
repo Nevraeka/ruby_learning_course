d67034d44147b2dffa34de8b0d50d135289c862f
Erik Isaksen <erik_isaksen@hotmail.com>
2012-03-11 09:44:09 -0400

    fixing sum method as per victors comments

--- sum.rb
+++ sum.rb
@@ -1,8 +1,12 @@
-def sum (arr)
-  arr.inject { | sum, i | sum + i }
+def make_integer_array (str)
+  str.split(",").map {| input_num | input_num.to_i}
+end
+
+def sum (nums)
+  make_integer_array(nums).compact.inject { | sum, i | sum + i }
 end
 
 puts "Enter an array with the numbers you wish to add together"
-input = eval(gets.chomp)
+input = gets.strip
 puts sum input
 
