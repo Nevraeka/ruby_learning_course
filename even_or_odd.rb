c2bf311ab3c99b21fcc652fd225ffe72074e0619
Erik Isaksen <erik_isaksen@hotmail.com>
2012-03-12 00:16:09 -0400

    updating even and odd to hide fixnum limitation

--- even_or_odd.rb
+++ even_or_odd.rb
@@ -1,5 +1,5 @@
 def even_or_odd (str)
-  str.split(/[^0-9]/).reject(&:empty?).map { | num | puts "The number #{num} is #{num.to_i % 2 == 0 ? 'even' : 'odd'}" }
+  str.split(/[^(0-9)|(.)]/).reject(&:empty?).map { | num | puts "The number #{num.to_i} is #{num.to_i.even? ? 'even' : 'odd'}" }
 end
 
 puts "Enter a set of numbers to see which ones are even and which are odd"
