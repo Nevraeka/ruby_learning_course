7d292d876bdb3aca92746a7ea4163fe4b5fccf2d
Erik Isaksen <erik_isaksen@hotmail.com>
2012-03-13 02:40:52 -0400

    updating even / odd example (with glue)

--- even_or_odd.rb
+++ even_or_odd.rb
@@ -1,7 +1,27 @@
-def even_or_odd (str)
-  str.split(/[^(0-9)|(.)]/).reject(&:empty?).map { | num | puts "The number #{num.to_i} is #{num.to_i.even? ? 'even' : 'odd'}" }
+def round_float (num)
+  num.to_f.round
+end
+
+def round_to_int (fl)
+  round_float(fl).to_i
+end
+
+def split_floats(str)
+  str.split(/[^(0-9)|(.)]/).reject(&:empty?)
+end
+
+def odd_even (num)
+ round_to_int(num).even? ? 'even' : 'odd'
+end
+
+def odd_or_even? (str)
+  split_floats(str).inject([]) { | a, num | a << { round_to_int(num) => odd_even(num)}; a }
+end
+
+def report_odd_or_even(input)
+ odd_or_even?(input).each {|h| h.each { |key, val| puts "#{key} is an #{val} number"} }
 end
 
 puts "Enter a set of numbers to see which ones are even and which are odd"
 input = gets.strip
-puts even_or_odd input
+report_odd_or_even(input) 
