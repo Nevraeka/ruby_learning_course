9764c5954d0a01a011af028852b44484487f76ff
Erik Isaksen <erik_isaksen@hotmail.com>
2012-03-21 00:35:32 -0400

    updating file swapper

--- swapper.rb
+++ swapper.rb
@@ -1,19 +1,52 @@
-# require 'tempfile'
-
-module File
+module FileSwapper
   class Swapper
-    def add_temp_file(name, file)
-      @#{name} = File.open(file, "r").read
+    
+    def initialize(*args)
+      arg_arr = args.to_a
+      exit_if_invalid(arg_arr)
+      @file_list = []
+      read_files(arg_arr)
+      write_files
     end
-    def remove_temp_file(name)
-      @#{name} = nil
+    
+    def read_files(files)
+      files.each do | f |
+        @file_list << { f => File.open(f, "r").read }
+      end
     end
-    def files_exist?(*files)
+
+    def write_files
+      rev_files = @file_list.reverse
+      @file_list.each_with_index do | f, i |
+        File.open(f.keys[0], "w"){ |file| 
+	  file << rev_files[i].values[0] 
+	  puts "writing file #{f.keys[0]} with content from #{rev_files[i].keys[0]}"
+	}
+      end
+    end
+   
+    def exit_if_invalid(args)
+      if !files_exist?(args)
+        puts "One or more of the files you stated does not exist"
+	return
+      end
+    end
+
+    def files_exist?(files)
       arr = []
       files.each do | file_path |
         arr << File.exists?(file_path)
       end
       !(arr.include? false)
     end
+
   end
 end
+
+
+puts "Enter the first file you wish to swap"
+first = gets.chomp
+puts "Enter the second file you wish to swap"
+second = gets.chomp
+
+FileSwapper::Swapper.new(first,second)
