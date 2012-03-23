e8750fe24b897b73c7cfcf1f846233f56d525a58
Erik Isaksen <erik_isaksen@hotmail.com>
2012-03-23 00:31:09 -0400

    updating dynamic method creation and overriding methods

--- dog.rb
+++ dog.rb
@@ -20,10 +20,34 @@ class Dog
     return "You caught the cat! Good job #{@name}!" if canine_temporal_concept >= 9    
     "He got away! You suck #{@name}"
   end
+
+  def teach_trick(trick, &trick_block)
+    unless respond_to?(trick)
+      define_singleton_method(trick, &trick_block)
+    else
+      puts "You already have a method #{trick} defined. Try using 'teach_trick!' to override."
+    end
+  end
+
+  def teach_trick!(trick, &trick_block)
+    remove_method trick if respond_to?(trick)
+    define_singleton_method(trick, &trick_block)
+  end
+
+  def method_missing(meth, *args, &block)
+    puts "The method #{meth} is not available to #{@name} as you have specified" unless respond_to?(meth)
+  end
 end
 
 d = Dog.new("Leo")
-d.bark
-d.eat
-d.chase_cat
-d.chase_cat
+d.teach_trick("spin"){ "#{@name} is spinning" }
+puts d.spin
+d.teach_trick("spin"){ "added this again" }
+puts d.spin
+puts d.run_away
+d.teach_trick!("run_away"){ "overrided method" }
+puts d.run_away
+# d.bark
+# d.eat
+# d.chase_cat
+# d.chase_cat
