181eb4191aeeb00a98483c899ef9a68feb3c0ec3
Erik Isaksen <erik_isaksen@hotmail.com>
2012-03-23 08:38:17 -0400

    refactoring based on victors feedback

--- dog.rb
+++ dog.rb
@@ -13,12 +13,8 @@ class Dog
   end
   
   def chase_cat
-    canine_temporal_concept = rand(20)
-    canine_temporal_concept.downto(1).each do
-      puts "Which way did he go?!!" 
-    end
-    return "You caught the cat! Good job #{@name}!" if canine_temporal_concept >= 9    
-    "He got away! You suck #{@name}"
+    (rand(20)).downto(1).each { puts "Which way did he go?!!" }
+    puts ["You caught the cat! Good job %s!","He got away! You suck %s!"][rand(2)] % @name
   end
 
   def teach_trick(trick, &trick_block)
@@ -47,7 +43,7 @@ puts d.spin
 puts d.run_away
 d.teach_trick!("run_away"){ "overrided method" }
 puts d.run_away
-# d.bark
-# d.eat
-# d.chase_cat
+d.bark
+d.eat
+d.chase_cat
 # d.chase_cat
