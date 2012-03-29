60195ae69565387cfa5f90318ff94c386fd0e7d5
Erik Isaksen <erik_isaksen@hotmail.com>
2012-03-29 07:11:08 -0400

    adding grandma updates. BROKEN - wip

--- e6_w3.rb
+++ e6_w3.rb
@@ -1,17 +1,56 @@
-puts "Please chat with Grandma Ruby."
+=begin
+doctest: random_year should return an inclusive array of years between 1930 and 1951"
+>> random_recollections.include?(1930)
+=> true
+>> random_recollections.include?(1951)
+=> true
+>> random_recollections.include?(1955)
+=> false
+doctest: 'random year' should return a year before or equal to 1951 
+>> random_year <= 1951
+=> true
+doctest: 'random_year' should return a year after or equal to 1930
+>> random_year >= 1930 
+=> true
+doctest: 'random_year' should return an Fixnum type 
+>> random_year.class
+=> Fixnum
+doctest: 'goodbye?' should return false if text is empty.
+>> goodbye?("")
+=> true
+doctest: 'goodbye?' should return false if text is uppercase.
+>> goodbye?("HELLO")
+=> false
+doctest: 'goodbye?' should return true if text is lowercased
+>> goodbye?("hello")
+=> true
+=end
 
-def deaf_grandma_chatter 
-  random_recollection = (1930..1951).to_a
+puts "Please chat with Grandma Ruby"
+
+def deaf_grandma_chatter
   loop do
-    puts "\>"
-    input = gets.chomp
-    break if input == "bye".upcase
-    if (input != input.upcase || input == "")
-      puts "HUH?! SPEAK UP, SONNY!"
-    else
-      puts "NO! NOT SINCE #{random_recollection[rand(random_recollection.size)]}!"
-    end 
-  end
+    input =  gets.strip
+    puts grandmas_response
+  end until goodbye?
+end
+
+def random_recollections
+  (1930..1951).to_a
+end
+
+def random_year
+ random_recollections[rand(random_recollections.size)]
+end
+
+def goodbye?
+  (gets.chomp != gets.chomp.upcase! || gets.chomp == "")
+end
+
+def grandmas_response
+  p goodbye?
+
+  goodbye? ? "HUH?! SPEAK UP, SONNY!" : "NO! NOT SINCE #{random_year}!" 
 end
 
 deaf_grandma_chatter
